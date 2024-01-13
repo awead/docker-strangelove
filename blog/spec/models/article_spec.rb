ba780676b4e6e7212caacbf17d23f6866a043c84
Adam Wead <awead@users.noreply.github.com>
2024-01-13 00:14:50 -0500

    Part 9: Refactoring

--- blog/spec/models/article_spec.rb
+++ blog/spec/models/article_spec.rb
@@ -4,6 +4,28 @@ RSpec.describe Article, type: :model do
   describe "table" do
     it { is_expected.to have_db_column(:title) }
     it { is_expected.to have_db_column(:body) }
+    it { is_expected.to have_db_column(:status) }
     it { is_expected.to have_many(:comments) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:body) }
+    it { is_expected.to validate_length_of(:body).is_at_least(10) }
+    it { is_expected.to validate_inclusion_of(:status).in_array(described_class::VALID_STATUSES) }
+  end
+
+  describe "#archived?" do
+    context "when status is 'archived'" do
+      subject { described_class.new(status: "archived") }
+
+      it { is_expected.to be_archived }
+    end
+
+    context "when status is not 'archived'" do
+      subject { described_class.new }
+
+      it { is_expected.not_to be_archived }
+    end
   end
 end
--- blog/spec/models/comment_spec.rb
+++ blog/spec/models/comment_spec.rb
@@ -1,9 +1,27 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  require "rails_helper"
   describe "table" do
     it { is_expected.to have_db_column(:body) }
+    it { is_expected.to have_db_column(:status) }
     it { is_expected.to belong_to(:article) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_inclusion_of(:status).in_array(described_class::VALID_STATUSES) }
+  end
+
+  describe "#archived?" do
+    context "when status is 'archived'" do
+      subject { described_class.new(status: "archived") }
+
+      it { is_expected.to be_archived }
+    end
+
+    context "when status is not 'archived'" do
+      subject { described_class.new }
+
+      it { is_expected.not_to be_archived }
+    end
   end
 end
